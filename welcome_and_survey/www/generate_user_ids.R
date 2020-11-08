d8a1562b55a7537573fa88a38ec72cb87479016b
Jonathan Trattner <accounts@jdtrat.com>
2020-11-07 20:15:21 -0500

    Updated username functions to include adding and removing.

--- welcome_and_survey/www/generate_user_ids.R
+++ welcome_and_survey/www/generate_user_ids.R
@@ -66,6 +66,13 @@ setup_usernames <- function(drop_path, num_usernames) {
   group_a <- paste0(strings[1:(num_usernames/2),], "_GA")
   group_b <- paste0(strings[((num_usernames/2) + 1):num_usernames,], "_GB")
   
+  all_usernames <- data.frame(combined_groups = c(group_a, group_b))
+  
+  saveRDS(all_usernames, "all_usernames.rds")
+  rdrop2::drop_upload(file = 'all_usernames.rds',
+                      path = drop_path)
+  unlink("all_usernames.rds")
+  
   # Save the vectors to rds, upload to dropbox, and delete local versions
   saveRDS(group_a, "group_a.rds")
   rdrop2::drop_upload(file = 'group_a.rds', 
@@ -136,3 +143,138 @@ get_username <- function(group, drop_path) {
   return(username)
   
 }
+
+#' Add Usernames
+#'
+#' @param drop_path The path (folder) in dropbox account where the username file is located
+#' @param num_usernames The number of usernames to add.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_usernames <- function(drop_path, num_usernames) {
+  
+  if (num_usernames %% 2 != 0) {
+    stop("Number of usernames must be divisible by two.")
+  }
+  
+  # There is a better way to do this with functional programming, I'm sure, but
+  # this works...
+  
+  all_filename <- paste0(drop_path, "all_usernames.rds")
+  a_filename <- paste0(drop_path, "group_a.rds")
+  b_filename <- paste0(drop_path, "group_b.rds")
+  
+  rdrop2::drop_download(all_filename)
+  rdrop2::drop_download(a_filename)
+  rdrop2::drop_download(b_filename)
+  
+  original_usernames <- readRDS("all_usernames.rds")
+  original_group_a <- readRDS("group_a.rds")
+  original_group_b <- readRDS("group_b.rds")
+  
+  new_usernames <- rand_str(n = num_usernames, upperalpha = FALSE) %>%
+    dplyr::mutate(group = rep(c("_GA", "_GB"), (num_usernames/2))) %>%
+    tidyr::unite(col = combined_groups,
+                 sep = "")
+  
+  new_group_a <- new_usernames %>%
+    dplyr::filter(stringr::str_detect(combined_groups, "_GA")) %>%
+    dplyr::pull(combined_groups)
+  
+  new_group_b <- new_usernames %>%
+    dplyr::filter(stringr::str_detect(combined_groups, "_GB")) %>%
+    dplyr::pull(combined_groups)
+  
+  added_a <- c(original_group_a, new_group_a)
+  
+  added_b <- c(original_group_b, new_group_b)
+  
+  added_all_usernames <- rbind(original_usernames, new_usernames)
+  
+  files_usernames <- list(added_a,
+                          added_b,
+                          added_all_usernames)
+  
+  filenames_usernames <- c("group_a.rds",
+                           "group_b.rds",
+                           "all_usernames.rds")
+  
+  purrr::walk2(.x = files_usernames, 
+               .y = filenames_usernames,
+               ~saveRDS(object = .x, 
+                        file = .y))
+  
+  purrr::walk(.x = filenames_usernames, 
+              ~ rdrop2::drop_upload(file = .x,
+                                    path = drop_path))
+  
+  purrr::walk(.x = filenames_usernames, ~ unlink(.x))
+  
+}
+
+
+
+#' Remove usernames
+#' 
+#' @param drop_path 
+#' @param num_usernames The number of usernames to remove.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+remove_usernames <- function(drop_path, num_usernames) {
+  
+  if (num_usernames %% 2 != 0) {
+    stop("Number of usernames must be divisible by two.")
+  }
+  
+  # There is a better way to do this with functional programming, I'm sure, but
+  # this works...
+  
+  all_filename <- paste0(drop_path, "all_usernames.rds")
+  a_filename <- paste0(drop_path, "group_a.rds")
+  b_filename <- paste0(drop_path, "group_b.rds")
+  
+  rdrop2::drop_download(all_filename)
+  rdrop2::drop_download(a_filename)
+  rdrop2::drop_download(b_filename)
+  
+  original_usernames <- readRDS("all_usernames.rds")
+  original_group_a <- readRDS("group_a.rds")
+  original_group_b <- readRDS("group_b.rds")
+  
+  new_usernames <- data.frame(combined_groups = original_usernames[1:(nrow(original_usernames) - num_usernames),])
+  
+  new_group_a <- original_group_a[1:(length(original_group_a) - (num_usernames/2))]
+  
+  new_group_b <- original_group_b[1:(length(original_group_b) - (num_usernames/2))]
+  
+  files_usernames <- list(new_group_a,
+                          new_group_b,
+                          new_usernames)
+  
+  filenames_usernames <- c("group_a.rds",
+                           "group_b.rds",
+                           "all_usernames.rds")
+  
+  purrr::walk2(.x = files_usernames, 
+               .y = filenames_usernames,
+               ~saveRDS(object = .x, 
+                        file = .y))
+  
+  purrr::walk(.x = filenames_usernames, 
+              ~ rdrop2::drop_upload(file = .x,
+                                    path = drop_path))
+  
+  purrr::walk(.x = filenames_usernames, ~ unlink(.x))
+  
+}
+
+
+
+
+
+
