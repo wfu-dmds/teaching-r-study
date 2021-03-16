1ae8bf38a45c9cd23eb0918b046888fd8e00c838
nuripark10 <parkn17@wfu.edu>
2021-03-15 23:38:54 -0400

    data frame

--- pilot-analysis/analysis.R
+++ pilot-analysis/analysis.R
@@ -1,8 +1,7 @@
 library(rdrop2)
 library(tidyverse)
 # read in the dropbox token
-token <- readRDS("token.rds")
-drop_auth(rdstoken = "token.rds")
+drop_auth(rdstoken = "pilot-analysis/token.rds")
 
 ## list the files in the learnr_results folder
 files <- drop_dir("/D'Agostino McGowan Data Science Lab/learnr_results/")
@@ -22,3 +21,8 @@ d <- map_df(files$path_lower, f)
 ## make this a wider data frame with a column for each question per assessment per user
 ## In the end, we'd have 1 or two rows per username (depending on whether they completed the second assessment)
 ## The exploratory analysis could subset to only assessment 1 responses and then only assessment 2 responses by arm (A or B) and see if there is a differece
+## Score each questions
+
+d <- d %>%
+  pivot_wider(names_from = question, values_from= response)
+
